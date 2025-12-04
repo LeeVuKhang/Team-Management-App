ed84e91e4b6c62263706248241394935bef60f49
LeeVuKhang <lvukhangpq@gmail.com>
2025-12-04 13:08:07 +0700

    use react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './homepage.jsx'
 
 function App() {
-  return <Homepage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Đường dẫn mặc định "/" sẽ hiện trang homepage*/}
+        <Route path="/" element={<Homepage />} />
+        
+        {/* Đường dẫn "/welcome" sẽ hiện trang Welcome
+        <Route path="/welcome" element={<Welcome />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
